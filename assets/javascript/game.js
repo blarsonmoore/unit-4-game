a2bc0f931b51b7f37fe52b18c28de73ffc4e7d16
blarsonmoore <blarsonmoore@gmail.com>
2018-05-10 11:00:53 -0600

    js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,12 @@
 var randomNumber = "";
 var gameScore = 0;
-
 var winCount = 0;
 var lossCount = 0;
+var output = "";
+var userClick = "";
 
 
 $(document).ready(function () {
-
-  var winCount = 0;
-  var lossCount = 0;
 
   function randomNumber(min, max) {
     min = Math.ceil(19);
@@ -18,47 +16,6 @@ $(document).ready(function () {
 
   $("#random-number").html(randomNumber);
 
-  // var randomCrystalOne = Math.floor(Math.random() * 12) + 1;
-  // var randomCrystalTwo = Math.floor(Math.random() * 12) + 1;
-  // var randomCrystalThree = Math.floor(Math.random() * 12) + 1;
-  // var randomCrystalFour = Math.floor(Math.random() * 12) + 1;
-
-
-// var crystals = ["crystalOne", "crystalTwo", "crystalThree", "crystalFour"];
-
-// for (var i = 0; i < crystals.length; i++){
-//   var random = Math.floor(Math.random() * 12) + 1;
-//   $(".img-thumbnail").on("click", function(){
-//     var crystalOne = (crystals[0]);
-//   crystals[i] = random;
-//   console.log(random);
-//   });
-// }
-
-// $("#crystal-one").on("click", function(){
-//   var crystalOne = (crystals[0]);
-//   console.log(crystalOne);
-//   gameScore += crystalOne;
-//     console.log(gamscore);
-// });
-// $("#crystal-two").on("click", function(){
-//   var crystalTwo = (crystals[1]);
-//   console.log(crystalTwo);
-// });
-// $("#crystal-three").on("click", function(){
-//   var crystalThree = (crystals[2]);
-//   console.log(crystalThree);
-// });
-// $("#crystal-four").on("click", function(){
-//   var crystalFour = (crystals[3]);
-//   console.log(crystalFour);
-// });
-
-   
-  
-
-
-
   var crystals = [0, 1, 2, 3];
 
 for (var i = 0; i < crystals.length; i++){
@@ -67,61 +24,53 @@ for (var i = 0; i < crystals.length; i++){
 
   console.log(random);
 }
-// $("#crystal-one").val(crystals[0]);
-// $("#crystal-two").val(crystals[1]);
-// $("#crystal-three").val(crystals[2]);
-// $("#crystal-four").val(crystals[3]);
 
-$("#crystal-one").on("click", function(){
-  var click = crystals[0];
-  $("#player-score").text(crystals[0] + gameScore);
-  gameScore += click;
-});
-
-$("#crystal-two").on("click", function(){
-  var click = crystals[1];
-  $("#player-score").text(crystals[1] + gameScore);
-  gameScore += click;
-});
-
-$("#crystal-three").on("click", function(){
-  var click = crystals[2];
-  $("#player-score").text(crystals[2] + gameScore);
-  gameScore += click;
-});
-
-$("#crystal-four").on("click", function(){
-  var click = crystals[3];
-  $("#player-score").text(crystals[3] + gameScore);
-  gameScore += click;
-});
-console.log(crystals[0]);
-
-
-console.log(crystals[1]);
-
-
-console.log(crystals[2]);
-
-
-console.log(crystals[3]);
-
-
-if (gameScore === randomNumber) {
-  alert("You Wind");
-}
-
-//   gameScore += value;
-
-//   console.log(gameScore);
-
+$(".container").on("click", ".img-thumbnail", function () {
   
-// // $("#crystal-two").on("click", function(){
-// //   var value2 = (crystals[1]);
-// //   gameScore += value2;
+ 
+  if ($(".img-thumbnail").value == A) {
+    $("#player-score").text(crystals[0] + gameScore);
+  }
+  // if (this.value === output) {
+  //   $("#player-score").text(crystals[1] + gameScore);
+  // }
+  // if (this.value === output) {
+  //   $("#player-score").text(crystals[2] + gameScore);
+  // }
+  // if (this.value === output) {
+  //   $("#player-score").text(crystals[3] + gameScore);
+  // }
 
-//   $("#player-score").text(gameScore);
 
+
+
+// $("#crystal-one").on("click", function(){
+//   var click = crystals[0];
+//   $("#player-score").text(crystals[0] + gameScore);
+//   gameScore += click;
+// });
+// $("#crystal-two").on("click", function(){
+//   var click = crystals[1];
+//   $("#player-score").text(crystals[1] + gameScore);
+//   gameScore += click;
 // });
 
+// $("#crystal-three").on("click", function(){
+//   var click = crystals[2];
+//   $("#player-score").text(crystals[2] + gameScore);
+//   gameScore += click;
+// });
+
+// $("#crystal-four").on("click", function(){
+//   var click = crystals[3];
+//   $("#player-score").text(crystals[3] + gameScore);
+//   gameScore += click;
+// });
+ 
+if (gameScore === randomNumber) {
+  alert("You Win");
+}
+
 });
+});
+
